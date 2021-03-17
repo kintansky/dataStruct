562b5a3b227dfd34504d2ee880e131b43f6e9359
hebe <jx_tan@outlook.com>
2021-03-17 22:51:53 +0800

    add doule rotate

--- tree/avl/main.go
+++ tree/avl/main.go
@@ -26,6 +26,31 @@ func (n *Node) Height() (h int) {
 	return int(math.Max(float64(n.left.Height()), float64(n.right.Height()))) + 1
 }
 
+// RotateLeft 左旋
+func (n *Node) RotateLeft() {
+	// 1 以当前根的值复制出一个节点
+	newNode := NewNode(n.data)
+	// 2 这个复制节点的左子树指向原根的左子树
+	newNode.left = n.left
+	// 3 这个复制节点的右子树指向原根的右子树的左子树
+	newNode.right = n.right.left
+	// 4 替换原根节点的值为右节点的值
+	n.data = n.right.data
+	// 5 原根的右子树指向原根的右子树的右子树
+	n.right = n.right.right
+	// 6 原根的左子树指向复制节点
+	n.left = newNode
+}
+
+func (n *Node) RotateRight() {
+	newNode := NewNode(n.data)
+	newNode.right = n.right
+	newNode.left = n.left.right
+	n.data = n.left.data
+	n.left = n.left.left
+	n.right = newNode
+}
+
 type AvlTree struct {
 	root *Node
 }
@@ -46,6 +71,7 @@ func (a *AvlTree) insertNode(n *Node, pos *Node) {
 	} else {
 		fmt.Printf("node %v exists\n", n.data)
 	}
+	a.Rotate()
 }
 
 func (a *AvlTree) InsertNode(n *Node) {
@@ -74,13 +100,35 @@ func (a *AvlTree) walk(n *Node) {
 	}
 }
 
+func (a *AvlTree) Rotate() {
+	if a.root.right.Height()-a.root.left.Height() > 1 {
+		if a.root.right != nil && a.root.right.left.Height() > a.root.left.Height() {
+			a.root.right.RotateRight()
+			a.root.RotateLeft()
+		} else {
+			a.root.RotateLeft()
+		}
+		fmt.Printf("root:%d, height:%d, leftTreeHeight:%d, rightTreeHeight:%d\n", a.root.data, a.root.Height(), a.root.left.Height(), a.root.right.Height())
+	}
+	if a.root.right.Height()-a.root.left.Height() < -1 {
+		if a.root.left != nil && a.root.left.right.Height() > a.root.right.Height() {
+			a.root.left.RotateLeft()
+			a.root.RotateRight()
+		} else {
+			a.root.RotateRight()
+		}
+		fmt.Printf("root:%d, height:%d, leftTreeHeight:%d, rightTreeHeight:%d\n", a.root.data, a.root.Height(), a.root.left.Height(), a.root.right.Height())
+	}
+}
+
 func main() {
-	arr := []int{4, 3, 6, 5, 7, 8}
+	// arr := []int{4, 3, 6, 5, 7, 8}
+	arr := []int{10, 11, 7, 6, 8, 9}
 	avl := AvlTree{}
 	for _, d := range arr {
 		avl.InsertNode(NewNode(d))
 	}
 	avl.Walk()
 	h := avl.root.Height()
-	fmt.Println(h)
+	fmt.Printf("root:%d, height:%d, leftTreeHeight:%d, rightTreeHeight:%d", avl.root.data, h, avl.root.left.Height(), avl.root.right.Height())
 }
