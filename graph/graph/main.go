fdb482072131afff4efb14c9540ee06b42379766
hebe <jx_tan@outlook.com>
2021-03-31 22:30:48 +0800

    create dfs

--- graph/graph/main.go
+++ graph/graph/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Graph struct {
-	arr       []string
-	vertexNum int
-	edges     [][]int // 顶点的邻接矩阵
+	arr       []string // 顶点数组，对应邻接矩阵的坐标
+	vertexNum int      // 顶点数目，限制邻接矩阵大小
+	edges     [][]int  // 顶点的邻接矩阵
 	edgesNum  int
 }
 
@@ -64,6 +64,7 @@ func (g *Graph) GetWeight(v1, v2 int) int {
 	return g.edges[v1][v2]
 }
 
+// 显示邻接矩阵
 func (g *Graph) Show() {
 	fmt.Println(" ", strings.Join(g.arr, " "))
 	for i, line := range g.edges {
@@ -75,12 +76,40 @@ func (g *Graph) Show() {
 	}
 }
 
+// 遍历，DFS深度优先
+func (g *Graph) DFS() {
+	visited := make([]bool, g.vertexNum)
+	for i := 0; i < g.vertexNum; i++ {
+		if visited[i] {
+			continue
+		}
+		g.dfs(i, visited)
+	}
+}
+
+func (g *Graph) dfs(i int, visited []bool) {
+	visited[i] = true
+	fmt.Println(g.GetVertex(i))
+	for j := 0; j < g.vertexNum; j++ {
+		if !visited[j] && g.edges[i][j] != 0 {
+			g.dfs(j, visited)
+		}
+	}
+}
+
 func main() {
 	g := NewGraph(5)
 	g.Insert("a")
 	g.Insert("b")
 	g.Insert("c")
-	g.AddEdge(g.GetVertexIdx("a"), g.GetVertexIdx("c"), 1)
-	g.AddEdge(g.GetVertexIdx("b"), g.GetVertexIdx("c"), 1)
+	g.Insert("d")
+	g.Insert("e")
+	// g.AddEdge(g.GetVertexIdx("a"), g.GetVertexIdx("c"), 1)
+	// g.AddEdge(g.GetVertexIdx("b"), g.GetVertexIdx("c"), 1)
+	g.AddEdge(g.GetVertexIdx("b"), g.GetVertexIdx("d"), 1)
+	g.AddEdge(g.GetVertexIdx("d"), g.GetVertexIdx("e"), 1)
+	g.AddEdge(g.GetVertexIdx("d"), g.GetVertexIdx("a"), 1)
 	g.Show()
+
+	g.DFS()
 }
